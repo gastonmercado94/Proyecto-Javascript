2fc90b6cd8eac4b1a5856c61bfebcd05d5c24369
Gaston Mercado <71989800+gastonmercado94@users.noreply.github.com>
2020-11-09 18:03:32 -0300

    Add files via upload

---
+++ js/scriptform.js
@@ -0,0 +1,23 @@
+function ChangeInputName(){
+    document.getElementById("input__name").style.border="1px solid lightskyblue";
+    document.getElementById("input__name").style.background="rgba(255,255,255,0.2)";
+}
+
+function ChangeInputLastname(){
+    document.getElementById("input__lastname").style.border="1px solid lightskyblue";
+    document.getElementById("input__lastname").style.background="rgba(255,255,255,0.2)";
+}
+
+function ChangeInputMail(){
+    document.getElementById("input__mail").style.border="1px solid lightskyblue";
+    document.getElementById("input__mail").style.background="rgba(255,255,255,0.2)";
+}
+
+function ChangeInputMsg(){
+    document.getElementById("input__msg").style.border="1px solid lightskyblue";
+    document.getElementById("input__msg").style.background="rgba(255,255,255,0.2)";
+}
+
+function SendAlert(){
+    alert("Your message has been sent");
+}
