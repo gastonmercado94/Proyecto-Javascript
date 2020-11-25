191a1287c1f12ecdc41a225047b4102d04656565
Gaston Mercado <71989800+gastonmercado94@users.noreply.github.com>
2020-11-25 16:14:28 -0300

    Tercer entrega

--- js/carrito.js
+++ js/carrito.js
@@ -1,88 +1,130 @@
-// variable con los productos de la abse de datos
-let products = getProducts();
-console.log(products);
+// variable con los productos de la base de datos
+getProducts();
 
 // creo array del carrito
+let packageCounter = 0 
+let cart = [];
 
-const cart = [];
-
-
-products.forEach( product => {
+function addProducts(products) {
+    products.forEach( product => {
     let productCard = createProductCard(product);
     $("#productsContainer").append(productCard);
  
 })
+}
 
 function createProductCard(product) {
+
     let productCard = document.createElement('div');
     productCard.id = product.id;
     productCard.classList = 'productCard';
     
-
-
-let nameDiv = document.createElement('div');
-nameDiv.innerHTML = product.name;
-nameDiv.classList = 'productName';
+    let nameDiv = document.createElement('div');
+    nameDiv.innerHTML = product.name;
+    nameDiv.classList = 'productName';
  
-let priceDiv = document.createElement('div');
-priceDiv.innerHTML = "Price: $" + product.price;
+    let priceDiv = document.createElement('div');
+    priceDiv.innerHTML = "Price: $" + product.price;
 
-let stockDiv = document.createElement('div');
-stockDiv.innerHTML = "Stock: " + product.stock;
+    let stockDiv = document.createElement('div');
+    stockDiv.innerHTML = "Stock: " + product.stock;
 
+    let inputQuantityDiv = document.createElement('div');
+    inputQuantityDiv.classList = "inputQuantityDiv";
 
-let addButton = document.createElement('button');
-addButton.innerHTML = "Add product";
-addButton.classList = 'addButton';
+    let inputQuantityName = document.createElement('div');
+    inputQuantityName.innerHTML = "Quantity: ";
 
-// $(".addButton").click(function(){
-
-addButton.addEventListener('click', ()=> {
-    cart.push(product);
-    console.log(cart);
-    localStorage.setItem('cart', JSON.stringify(cart));
+    let inputQuantity = document.createElement('input');
+    inputQuantity.classList = 'inputQuantity';
+    inputQuantity.type = "number";
 
 
-    let showcartdiv = document.createElement('div');
-    showcartdiv.classList = 'showcartdiv';
-    
-    let cartName = document.createElement('div');
-    cartName.classList = cartName;
-    cartName.innerHTML = product.name;
-    
-    let cartPrice = document.createElement('div');
-    cartPrice.innerHTML = "Price $" + product.price;
-
-    let cartQuantity = document.createElement('div');
-    cartQuantity.innerHTML = "Quantity:";
+    let addButton = document.createElement('button');
+    addButton.innerHTML = "Add product";
+    addButton.classList = 'addButton';
 
 
-    $("#showcart").append(showcartdiv);
-   
-    showcartdiv.appendChild(cartName);
-    showcartdiv.appendChild(cartPrice);
-    showcartdiv.appendChild(cartQuantity);
-
-   
-
-    // $(".showcartdiv").append(cartName);
-    // $(".showcartdiv").append(cartPrice);
-    // $(".showcartdiv").append(cartQuantity);
-
-
-});
-
-// $(".productCard").append(nameDiv);
-// $(".productCard").append(priceDiv);
-// $(".productCard").append(stockDiv);
-// $(".productCard").append(addButton);
+    addButton.addEventListener('click', ()=> {
+        if (inputQuantity.value<1){
+                alert("ingrese cantidad valida");
+        } else {
+        const package = new Package(product, inputQuantity.value, packageCounter);
+        packageCounter = packageCounter + 1;
+        cart.push(package);
+        console.log(cart);
+        localStorage.setItem('cart', JSON.stringify(cart));
+        showCart(cart);
 
 
-productCard.appendChild(nameDiv);
-productCard.appendChild(priceDiv);
-productCard.appendChild(stockDiv);
-productCard.appendChild(addButton);
+}});
+
+    productCard.appendChild(nameDiv);
+    productCard.appendChild(priceDiv);
+    productCard.appendChild(stockDiv);
+    productCard.appendChild(inputQuantityDiv);
+    inputQuantityDiv.appendChild(inputQuantityName);
+    inputQuantityDiv.appendChild(inputQuantity);
+    productCard.appendChild(addButton);
 
     return productCard;
 }
+
+function showCart(){
+
+    $("#showcart").remove();
+    var showcart =  document.createElement('div');
+    showcart.id = "showcart";
+    $("#mainShowCart").append(showcart);
+
+    cart.forEach( package => {
+
+        let showcartdiv = document.createElement('div');
+        showcartdiv.classList = 'showcartdiv';
+        
+        let cartName = document.createElement('div');
+        cartName.classList = cartName;
+        cartName.innerHTML = package.product.name;
+        
+        let cartPrice = document.createElement('div');
+        cartPrice.innerHTML = "Price $" + package.product.price;
+
+        let cartQuantity = document.createElement('div');
+        cartQuantity.innerHTML = "Quantity: " + package.quantity;
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "Remove";
+        removeButton.classList = "removeButton";
+        removeButton.id = package.product.id ;
+
+        removeButton.addEventListener('click', ()=> {
+            removePackage(package.id);
+
+        });
+
+        $("#showcart").append(showcartdiv);
+    
+        showcartdiv.appendChild(cartName);
+        showcartdiv.appendChild(cartPrice);
+        showcartdiv.appendChild(cartQuantity);
+        showcartdiv.appendChild(removeButton);
+
+    })
+}
+
+class Package {
+    constructor(product, quantity, id){
+        this.product = product;
+        this.quantity = quantity;
+        this.id = id;
+    }
+}
+
+function removePackage(packageId){
+    cart = cart.filter(package => package.id != packageId);
+    showCart();
+}
+
+
+
 
--- js/database.js
+++ js/database.js
@@ -8,17 +8,21 @@ class Product {
 }
 function getProducts(){
 
-    let productAsObjects = JSON.parse(data);
-    let products = [];
 
-    for(element of productAsObjects){
-        let product = new Product(element.id, element.name, element.price, element.stock);
-        products.push(product);
-    }
+    $.ajax({
+        url: "js/data.json",
+        dataType: "json",
+        type:"get",
+        success: function(response) {
+            console.log("asd; " , response);
+            let products = [];
+            for(element of response){
+                let product = new Product(element.id, element.name, element.price, element.stock);
+                products.push(product);
+            }
+            addProducts(products);
+        }
+       });
 
-    return products;
 }
 
-
-
-const data = '[{"id": 1,"name": "London Guide","price": 11,"stock": 27},{"id": 2,"name": "Tokyo guide","price": 15,"stock": 74}, {"id": 3,"name": "Paris guide","price": 11,"stock": 9},{"id": 4,"name": "Moscow guide","price": 13,"stock": 29},{"id": 5,"name": "Thailand guide","price": 10,"stock": 4}, {"id": 6,"name": "Patagonia guide","price": 21,"stock": 43}, {"id": 7,"name": "Iceland guide","price": 30,"stock": 50}, {"id": 8,"name": "Peru guide","price": 25,"stock": 96}]'
